3c550a0bfc9599e3d4cdadd004e83a3d6517b153
Will Robertson <wspr81@gmail.com>
2011-02-01 16:00:02 +1030

    fix comments for functions to-be-removed

--- elliptic123.m
+++ elliptic123.m
@@ -635,51 +635,9 @@ end
 % END FUNCTION ELLIPTIC12i()
 
 function [F,E,Z] = elliptic12ic(u,m,tol)
-
-% ELLIPTIC12ic evaluates the value of the Incomplete Elliptic Integrals 
-% of the First, Second Kind and Jacobi's Zeta Function.
 %
-%   [F,E,Z] = ELLIPTIC12(U,M,TOL) where U is a phase in radians, 0<M<1 is 
-%   the module and TOL is the tolerance (optional). Default value for 
-%   the tolerance is eps = 2.220e-16.
-%
-%   ELLIPTIC12 uses the method of the Arithmetic-Geometric Mean 
-%   and Descending Landen Transformation described in [1] Ch. 17.6,
-%   to determine the value of the Incomplete Elliptic Integrals 
-%   of the First, Second Kind and Jacobi's Zeta Function [1], [2].
-%
-%       F(phi,m) = int(1/sqrt(1-m*sin(t)^2), t=0..phi);
-%       E(phi,m) = int(sqrt(1-m*sin(t)^2), t=0..phi);
-%       Z(phi,m) = E(u,m) - E(m)/K(m)*F(phi,m).
-%
-%   Tables generating code ([1], pp. 613-621):
-%       [phi,alpha] = meshgrid(0:5:90, 0:2:90);                  % modulus and phase in degrees
-%       [F,E,Z] = elliptic12(pi/180*phi, sin(pi/180*alpha).^2);  % values of integrals
-%
-%   See also ELLIPKE, ELLIPJ, ELLIPTIC12I, ELLIPTIC3, THETA, AGM.
-%
-%   References:
-%   [1] M. Abramowitz and I.A. Stegun, "Handbook of Mathematical Functions", 
-%       Dover Publications", 1965, Ch. 17.1 - 17.6 (by L.M. Milne-Thomson).
-%   [2] D. F. Lawden, "Elliptic Functions and Applications"
-%       Springer-Verlag, vol. 80, 1989
-
-% GNU GENERAL PUBLIC LICENSE Version 2, June 1991
-% http://www.gnu.org/licenses/gpl.html 
-% Everyone is permitted to copy and distribute verbatim copies of this 
-% script under terms and conditions of GNU GENERAL PUBLIC LICENSE. 
-%  
-% Copyright (C) 2007 by Moiseev Igor. All rights reserved.
-% 34106, SISSA, via Beirut n. 2-4,  Trieste, Italy
-% For support, please reply to 
-%     moiseev[at]sissa.it, moiseev.igor[at]gmail.com
-%     Moiseev Igor, 
-%     34106, SISSA, via Beirut n. 2-4,  Trieste, Italy
-%
-% The code is optimized for ordered inputs produced by the functions 
-% meshgrid, ndgrid. To obtain maximum performace (up to 30%) for singleton, 
-% 1-dimensional and random arrays remark call of the function unique(.) 
-% and edit further code. 
+% Bug fix for the elliptic12 in the main distribution.
+% This function should disappear when the fixes appear there.
 
 if nargin<3, tol = eps; end
 if nargin<2, error('Not enough input arguments.'); end
@@ -778,6 +736,9 @@ end
 
 
 function Pi = elliptic3ic(u,m,c)
+%
+% Bug fix for the elliptic12 in the main distribution.
+% This function should disappear when the fixes appear there.
 
 if nargin<3, error('Not enough input arguments.'); end
 if ~isreal(u) | ~isreal(m) | ~isreal(c)
